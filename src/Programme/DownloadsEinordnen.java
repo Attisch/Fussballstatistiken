762462a7b2cd8e7d4596fb8608d3434d21f8eafa
Attisch <60990960+Attisch@users.noreply.github.com>
2020-03-16 19:28:22 +0100

    Update DownloadsEinordnen.java

--- src/Programme/DownloadsEinordnen.java
+++ src/Programme/DownloadsEinordnen.java
@@ -11,7 +11,6 @@ public class DownloadsEinordnen {
 
 	public static void main(String[] args) {
 		Speicherzugriff.Datei.downloadsEinordnen(Verwendbare.DOWNLOADS_VON, Verwendbare.DOWNLOADS_NACH);
-	//	Speicherzugriff.Datei.downloadsEinordnen("C:\\Users\\user\\Desktop\\Programmierung\\Downloads", "Daten");
 	}
 
 }
