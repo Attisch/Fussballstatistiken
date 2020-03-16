959ecf69859c9696abdaaf20ba8d378b49dce903
Attisch <60990960+Attisch@users.noreply.github.com>
2020-03-16 19:26:28 +0100

    Update Wettbewerb.java

--- src/Fussball/Wettbewerb.java
+++ src/Fussball/Wettbewerb.java
@@ -37,64 +37,7 @@ public class Wettbewerb {
 		return info.split(" → ", 3)[2];
 	}
 	
-	/**
-	 * Eine {@link Spielprozedur} durchläuft die Spiele vom {@link #index Index} bis von der Spielprozedur festgelegtem Ende.
-	 * @param prozedur
-	 * @throws Fehler
-	 */
-/*	public void durchlaufeSpiele (Spielprozedur prozedur) throws Fehler {
-		Spielprozedurorder order = Spielprozedurorder.KEIN;
-		do {
-			for (Spiel spiel : khwSpiele.spiele) {
-				order = prozedur.benutzt(spiel);
-				if (order != Spielprozedurorder.NÄCHSTES_SPIEL) 
-					break;
-			}
-		} while (order !=Spielprozedurorder.ENDE && nächsterKhwIndex());
-	}*/
-	
-	
-	/**
-	 * Eine {@link Wettbewerbsprozedur} durchläuft die Spiele.
-	 * @param prozedur
-	 * @throws Fehler
-	 */
-/*	public void durchlaufeSpiele (Wettbewerbsprozedur prozedur) throws Fehler {
-		Spiel spiel = null;
-		try {
-			Spielprozedurorder order = Spielprozedurorder.NÄCHSTES_SPIEL;
-			Jahr jahr;
-			Monat monat;
-			KhwSpiele khwSpiele;
-			do {
-				jahr = jahre[prozedur.index().jahr];
-				prozedur.khw().jahr = jahr.zahl;
-				do {
-					monat = jahr.monate[prozedur.index().monat];
-					prozedur.khw().monat = monat.zahl;
-					do {
-						khwSpiele = monat.khwSpiele[prozedur.index().khw];
-						prozedur.khw().zahl = khwSpiele.khwZahl;
-						prozedur.khwSpiele (khwSpiele);
-						do {
-							spiel = khwSpiele.spiele[prozedur.index().spiel];
-							order = prozedur.benutzt(spiel);
-						} while (order.compareTo(Spielprozedurorder.NÄCHSTES_SPIEL) <= 0 || ++prozedur.index().spiel< khwSpiele.spiele.length);
-						prozedur.index().spiel = 0;		// In der neuen Khw beim ersten Spiel anfangen
-					} while (order.compareTo(Spielprozedurorder.NÄCHSTE_KHW) <=0 || ++prozedur.index().khw< monat.khwSpiele.length);
-					prozedur.index().khw = 0;		// Im neuen Monat in der ersten Khw anfangen
-				} while (order.compareTo(Spielprozedurorder.NÄCHSTER_MONAT) <=0 || ++prozedur.index().monat< jahr.monate.length);
-				prozedur.index().monat = 0;		// Im neuen Jahr im ersten Monat anfangen
-			} while (order !=Spielprozedurorder.ENDE || ++prozedur.index().jahr< jahre.length);
-			prozedur.ende();
-		} catch (Exception e) {
-			String meldung = "Fehler beim Durchlaufen der Spiele des Wettbewerbs '" +info +"' aufgetreten. ";
-			if (spiel != null)
-				meldung += "Aktuelles Spiel: " +spiel.toString() +" ";
-			throw new Fehler (meldung +Fehler.entstehung(e));
-		}
-	}*/
-	
+
 	public void durchlaufeSpiele (KhwProzedur prozedur) throws Fehler {
 		Spielprozedurorder order = Spielprozedurorder.KEIN;
 		KhwSpiele khwSpiele = null;
@@ -143,29 +86,6 @@ public class Wettbewerb {
 		return jahre[index.jahr].monate[index.monat].khwSpiele[index.khw];
 	}
 	
-	/**
-	 * @return true, wenn der Index der Kalenderhalbwoche zum nächsten gehen konnte. Ansonten false und der Index wird zum Ausgangsindex zurückgesetzt.
-	 */
-/*	public boolean nächsterKhwIndex() {
-		KhwIndex neuerIndex = index;
-		byte indexNr = neuerIndex.khw;
-		if (++indexNr >= jahre[index.jahr].monate[index.monat].khwSpiele.length) {
-			neuerIndex.khw = 0;
-			indexNr = neuerIndex.monat;
-			if (++indexNr >= jahre[index.jahr].monate.length) {
-				neuerIndex.monat = 0;
-				indexNr = neuerIndex.jahr;
-				if (++indexNr >= jahre.length) {
-					neuerIndex.zurücksetzung();
-					return false;
-				}
-				else neuerIndex.jahr++;
-			}
-			else neuerIndex.monat++;
-		}
-		else neuerIndex.khw++;
-		return true;
-	}*/
 	
 	public String toString() {
 		String text = "\n" +info.toString()+" mit "+teamnamen.anzahl()+" Teilnehmer: \n"+teamnamen.toString()+"\n";
